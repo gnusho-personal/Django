435d97144b6cc75f6510b5dd512b012454509922
성준오 <seongjun-o@seongjun-oui-MacBookAir.local>
2021-06-30 11:15:52 +0900

    수정

--- smartkey/server/server/settings.py
+++ smartkey/server/server/settings.py
@@ -90,9 +90,6 @@ DATABASES = {
         'PASSWORD': 'knocktalkHYWEP',
         'HOST': 'hywep-rds.c8jl7lyhcvv7.us-east-2.rds.amazonaws.com',
         'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
     }
 }
 
