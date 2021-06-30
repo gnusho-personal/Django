5afd2b972593c26a6f4cc619192e3576d3c5a94d
성준오 <seongjun-o@seongjun-oui-MacBookAir.local>
2021-06-30 10:54:42 +0900

    mysql 재설정

--- smartkey/server/server/settings.py
+++ smartkey/server/server/settings.py
@@ -84,8 +84,15 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hywep-rds',
+        'USER': 'admin',
+        'PASSWORD': 'knocktalkHYWEP',
+        'HOST': 'hywep-rds.c8jl7lyhcvv7.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
