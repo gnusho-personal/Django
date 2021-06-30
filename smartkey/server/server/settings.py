ff459e627e489f395fb4f540c326b535948ac82f
성준오 <seongjun-o@seongjun-oui-MacBookAir.local>
2021-06-30 15:09:00 +0900

    formatter 추가

--- smartkey/server/server/settings.py
+++ smartkey/server/server/settings.py
@@ -62,10 +62,17 @@ MIDDLEWARE = [
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'format1': {
+            'fortmat': '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s %(message)s'
+            'datefmt': '%d/%b/%Y %H:%M:%S',
+        },
+    }
     'handlers': {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
+            'formatter': 'format1',
         },
     },
     'loggers': {
