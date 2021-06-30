0fad5bf88a6d2304dc13c57cd9a0014cffc02275
성준오 <seongjun-o@seongjun-oui-MacBookAir.local>
2021-06-30 15:47:49 +0900

    middleware.py 수정, 전에 만들어놓은 함수 추가

--- smartkey/server/server/middleware.py
+++ smartkey/server/server/middleware.py
@@ -12,13 +12,74 @@ class ResponseFormattingMiddleware:
         ]
 
     def __call__(self, request):
+        self.print_request_log(request)
         response = None
         if not response:
             response = self.get_response(request)
         if hasattr(self, 'process_response'):
             response = self.process_response(request, response)
-        print(request.headers)
+        self.print_response_log(response)
         return response
+
+    def json_default(self, value): 
+        if isinstance(value, datetime.date): 
+            return value.strftime('%Y-%m-%d')
+        elif isinstance(value, tuple):
+            return ''.join(value)
+        elif isinstance(value, bool):
+            return str(value)
+        elif isinstance(value, object):
+            return str(value)
+        raise TypeError('not JSON serializable')
+
+    def print_request_log(self, request):
+
+        print('Request Start')
+    
+        full_url = str(request.method) + str(request.get_full_path())
+        print('URL: ', full_url)
+
+        header_dict = {}
+        for name in request.headers:
+            header_dict[name] = request.headers[name]
+
+        json_header = json.dumps(header_dict, default=self.json_default, indent='\t')
+        print('Header: ', json_header)
+        # Meta에는 header의 모든 내용이 저장되있음
+        # 위의 코드를 사용시 모든 item들을 tuple 형태로 print할 수 있음
+
+        b = request.body.decode('utf-8')
+        if len(b) == 0: b = '{}'
+        json_body = json.loads(b)
+        print('Body: ', json.dumps(json_body, indent='\t'))
+        # 보통은 serializer를 이용해서 구현
+        # 현재는 model이 만들어지지 않았으니까 우선은 이렇게 사용
+
+        cookie_dict = {}
+        for name in request.COOKIES:
+            cookie_dict[name] = request.COOKIES[name]
+        json_cookie = json.dumps(cookie_dict, default=self.json_default, indent='\t')
+        print('Cookie: ', json_cookie)
+
+        print('Time: ', datetime.datetime.now())
+        print('Request Done\n')
+
+    def print_response_log(self, response):
+        print('Response Done')
+
+        header_dict = {}
+        for name in response.headers:
+            header_dict[name] = response.headers[name]
+
+        json_header = json.dumps(header_dict, default=self.json_default, indent='\t')
+        print('Header: ', json_header)
+
+        print('Code: ', response.status_code)
+
+        print('Body: ', response.content)
+
+        print('Time: ', datetime.datetime.now())
+        print('\n')
 
     def process_response(self, request, response):
         """
