8535b8ef7e637eb53cd840d8f8db2afdb7d011ad
성준오 <seongjun-o@seongjun-oui-MacBookAir.local>
2021-07-01 11:08:41 +0900

    middleware 파일 내부에서 Logging t설정

--- smartkey/server/server/middleware.py
+++ smartkey/server/server/middleware.py
@@ -5,24 +5,22 @@ from logging.handlers import RotatingFileHandler
 
 path = '/home/ubuntu/knocktalkHYWEP/smartkey/server/debug.log'
 
-logger = logging.getLogger('django.request')
+logger = logging.getLogger('RESTapiLogging')
 
 formatter = logging.Formatter('Time & Level: %(asctime)s;%(levelname)s\n %(message)s')
 
 # log를 console에 print하는 handler 설정
-'''
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.INFO)
 stream_handler.setFormatter(formatter)
-'''
 
 # log를 file에 저장하면서 maxbytes만큼만 저장하고 다른 파일에 내용을 백없하는 rotatingfilehandler 설정
 # backupcout 만큼 backupfile 생성 // debug.log에 .(숫자) 넣어서 파일 만들어줌 필요하면 자동으로 숫자를 뒤로 밀어줌
 file_handler = RotatingFileHandler(path, maxBytes = 1024, backupCount = 5)
 file_handler.setLevel(logging.INFO)
-#file_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
 
-#logger.addHandler(stream_handler)
+logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
 class LoggingMiddleware:
@@ -74,6 +72,7 @@ class LoggingMiddleware:
             header_dict[name] = request.headers[name]
 
         json_header = json.dumps(header_dict, default=self.json_default, indent='\t')
+        logger.info(json_header)
         #print('Header: ', json_header)
         # Meta에는 header의 모든 내용이 저장되있음
         # 위의 코드를 사용시 모든 item들을 tuple 형태로 print할 수 있음
