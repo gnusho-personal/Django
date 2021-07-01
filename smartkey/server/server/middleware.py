ceebbe7bbfa6a85b26aab449c192abbbfd39dd04
성준오 <seongjun-o@seongjun-oui-MacBookAir.local>
2021-07-01 16:57:40 +0900

    router 완성 전부터 시작 및 log 처리 관련 에러 확인할 것

--- smartkey/server/server/middleware.py
+++ smartkey/server/server/middleware.py
@@ -18,7 +18,7 @@ stream_handler.setFormatter(formatter)
 # backupcout 만큼 backupfile 생성 // debug.log에 .(숫자) 넣어서 파일 만들어줌 필요하면 자동으로 숫자를 뒤로 밀어줌
 
 #file_handler = RotatingFileHandler(path, maxBytes = 1024 * 4, backupCount = 5)
-file_handler = TimedRotatingFileHandler(path, when = 'M', interval = 5, backupCount = 7)
+file_handler = TimedRotatingFileHandler(path, when = 'midnight', interval = 1, backupCount = 7)
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(formatter)
 
--- smartkey/server/testDB/views.py
+++ smartkey/server/testDB/views.py
@@ -24,3 +24,61 @@ test_detail = TestViewSet.as_view({
     'patch': 'partial_update',
     'delete': 'destroy',
 })
+
+'''
+class TestDB(APIView):
+    # Post
+    def post(self, request):
+        # body에서 정보를 뽑아와서 object를 save할 예정
+        data = JSONParser().parse(request)
+        test_serializer = TestSerializer(data = data)
+
+        if test_serializer.is_valid():
+            test_serializer.save()
+            return HttpResponse(test_serializer.data, status = status.HTTP_200_OK)
+        else:
+            return HttpResponse(test_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    # Get
+    # Get /pk
+    def get(self, request, **kwargs):
+        if kwargs.get('pk') is None:
+            q = test_db.objects.all()
+            test_serializer = TestSerializer(data = q, many = True)
+            if test_serializer.is_valid():
+                test_serializer.save()
+                return HttpResponse(test_serializer.data, status = status.HTTP_200_OK)
+            else:
+                return HttpResponse(test_serializer.errors, status = status.HTTP_400_BAD_REQUEST)    
+        else:
+            pk = kwargs.get('pk')
+            q = test_db.objects.get(id = pk)
+            print("\n\n\n\n")
+            print(q)
+            print("\n\n\n\n")
+            test_serializer = TestSerializer(data = q)
+
+            if test_serializer.is_valid():
+                test_serializer.save()
+                return HttpResponse(test_serializer.data, status = status.HTTP_200_OK)
+            else:
+                return HttpResponse(test_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    # Put
+    def put(self, request, **kwargs):
+        
+
+        response = HttpResponse(ret, status = status.HTTP_200_OK)
+
+        return response
+
+    def delete(self, request, **kwargs):
+        if kwargs.get('pk') is None:
+            return HttpResponse("invalid request", status = status.HTTP_400_BAD_REQUEST)
+
+        else:
+            pk = kwargs.get('pk')
+            q = test_db.objects.get(id = pk)
+            q.delete()
+            return HttpResponse("delete " + pk + " ok!", status = status.HTTP_200_OK)
+'''
