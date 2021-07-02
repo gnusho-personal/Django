9f29fa0ccbcb5ed2a955bed03d720820cb928fa2
성준오 <seongjun-o@seongjun-oui-MacBookAir.local>
2021-07-02 09:25:43 +0900

    router 완성 전부터 시작 및 log 처리 관련 에러 확인할 것

--- smartkey/server/server/middleware.py
+++ smartkey/server/server/middleware.py
@@ -79,6 +79,8 @@ class LoggingMiddleware:
         # 보통은 serializer를 이용해서 구현
         # 현재는 model이 만들어지지 않았으니까 우선은 이렇게 사용
         #b = request.body.decode('utf-8')
+        body_tmp = request.body
+        if len(body_tmp) == 0: body_tmp = '{}'
         json_body_tmp = json.loads(request.body)
         json_body = json.dumps(json_body_tmp, indent='\t')
 
