71e28ab11a252f2d63255503c9818245d124612d
성준오 <seongjun-o@seongjun-oui-MacBookAir.local>
2021-07-08 15:07:12 +0900

    products model 추가

--- smartkey/server/info/models.py
+++ smartkey/server/info/models.py
@@ -3,6 +3,34 @@ from django.core.validators import RegexValidator
 from django.db import models
 import os
 
+class Contractors(models.Model):
+    '''
+    [시행사 model]
+    상호명: business name
+    주소: address
+    전화번호: call
+
+    callRegex는 call의 valid 여부 검사를 위한 정규표현식.
+    '''
+    business_name = models.CharField(max_length = 50)
+    address = models.CharField(max_length = 50)
+    callRegex = RegexValidator(regex = r'^\+?1?\d{8,15}$')
+    call = models.CharField(validators = [callRegex], max_length = 16, unique = True)
+
+class Sales_Agents(models.Model):
+    '''
+    판매 대리점 모델
+    상호명: business name
+    주소: address
+    전화번호: call
+
+    callRegex는 call의 valid 여부 검사를 위한 정규표현식입니다.
+    '''
+    business_name = models.CharField(max_length = 50)
+    address = models.CharField(max_length = 50)
+    callRegex = RegexValidator(regex = r'^\+?1?\d{8,15}$')
+    call = models.CharField(validators = [callRegex], max_length = 16, unique = True)
+
 class Products(models.Model):
     '''
     [상품 model]
@@ -43,33 +71,3 @@ class Products(models.Model):
 
     contractor = models.ForeignKey(Contractors, null = True, on_delete = models.SET_NULL)
     sales_agent = models.ForeignKey(Sales_Agents, null = True, on_delete = models.SET_NULL)
-
-
-class Contractors(models.Model):
-    '''
-    [시행사 model]
-    상호명: business name
-    주소: address
-    전화번호: call
-
-    callRegex는 call의 valid 여부 검사를 위한 정규표현식.
-    '''
-    business_name = models.CharField(max_length = 50)
-    address = models.CharField(max_length = 50)
-    callRegex = RegexValidator(regex = r'^\+?1?\d{8,15}$')
-    call = models.CharField(validators = [callRegex], max_length = 16, unique = True)
-
-class Sales_Agents(models.Model):
-    '''
-    판매 대리점 모델
-    상호명: business name
-    주소: address
-    전화번호: call
-
-    callRegex는 call의 valid 여부 검사를 위한 정규표현식입니다.
-    '''
-    business_name = models.CharField(max_length = 50)
-    address = models.CharField(max_length = 50)
-    callRegex = RegexValidator(regex = r'^\+?1?\d{8,15}$')
-    call = models.CharField(validators = [callRegex], max_length = 16, unique = True)
-
