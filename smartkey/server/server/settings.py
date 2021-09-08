04198a859b65a73ffcdf0dba9727daf4d6846afb
ohsungjoon <31716977+ohsungjoon@users.noreply.github.com>
2021-09-08 10:29:34 +0900

    [CHORE] 보안관련 부분 삭제
    해당 프로젝트를 public으로 돌리기위해서 database 관련된 부분을 전부 삭제함

--- smartkey/server/server/settings.py
+++ smartkey/server/server/settings.py
@@ -101,11 +101,11 @@ WSGI_APPLICATION = 'server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
-        'USER': 'admin',
-        'PASSWORD': 'knocktalkHYWEP',
-        'HOST': 'hywep-rds.c8jl7lyhcvv7.us-east-2.rds.amazonaws.com',
-        'PORT': '3306',
+        'NAME': '*******',
+        'USER': '*******',
+        'PASSWORD': '********',
+        'HOST': '********',
+        'PORT': '********',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         },
